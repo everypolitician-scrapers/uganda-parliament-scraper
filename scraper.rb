3b46766ba25268912ed711acee5ef9142509d184
Tony Bowden <tony@tmtm.com>
2016-04-08 11:12:22 +0100

    General scraper tweaks

--- scraper.rb
+++ scraper.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require 'scraperwiki'
 require "capybara/poltergeist"
 
@@ -10,8 +11,6 @@ def scrape(url)
   # we completely walk the list DOM then go to the individual mp pages so we can use the same Capybara session
   people.each do  |basic_details|
     more_details =  scrape_person(basic_details['source'], browser)
-    puts basic_details
-    puts more_details
     ScraperWiki.save_sqlite(['id'], basic_details.merge(more_details))
   end
 
@@ -26,16 +25,35 @@ def scrape_list(url,browser)
     browser.all('./tbody/tr[position()>2 and position()<last()]').each  do |row|
       absolute_uri = URI.join(url, row.find('./td/a')[:href]).to_s
       person = {}
+      names = row.find('./td[position()=1]').text.strip.split(/[[:space:]]/, 2).reverse
+      person[:name] = names.join(" ")
+      person[:given_name] = names.first
+      person[:family_name] = names.last
+      person[:sort_name] = "#{names.last}, #{names.first}"
+
       person['id'] = year + '-'+/&j=(?<id>\d*)&const/.match(absolute_uri)[:id].to_s
       person['source'] = absolute_uri
       person[:url] = absolute_uri
-      person[:name] = row.find('./td[position()=1]').text.strip
-      person[:area] = row.find('./td[position()=4]').text.strip
+      person[:district] = row.find('./td[position()=4]').text.strip
+      person[:constituency] = row.find('./td[position()=3]').text.strip
+
+      special = ["PWD", "YOUTH", "EX-OFFICIO", "Workers' Represantive", "Woman Representative", "UPDF"].to_set
+      if special.include? person[:constituency]
+        person[:post] = person[:constituency]
+        person[:constituency] = ""
+      end
       people.push(person)
     end
   end
   return people
 end
+
+def party_info(str)
+  party_info = /(.*)[[:space:]]*\((.*?)\)/.match(str)
+  binding.pry unless party_info
+  return party_info[1].strip, party_info[2].strip
+end
+
 
 def scrape_person (url, browser)
   person = {}
@@ -43,17 +61,17 @@ def scrape_person (url, browser)
   browser.within('//*/table/tbody') do
     person = {
         :image =>          URI.join(url, browser.find('./tr[position()=1]/td/img')[:src]).to_s,
-        :gender =>         browser.find(with_label 'Gender:').text.strip,
-        :martial_status => browser.find(with_label 'Marital Status:').text.strip,
+        :gender =>         browser.find(with_label 'Gender:').text.strip.downcase,
+        :martial_status => browser.find(with_label 'Marital Status:').text.strip.downcase,
         :email =>          browser.find(with_label 'Email address:').text.strip,
-        :postal_address => browser.find(with_label 'Postal Address:').text.strip,
+        :address =>        browser.find(with_label 'Postal Address:').text.strip,
         :phone =>          browser.find(with_label 'Mobile Telephone:').text.strip,
         :religion =>       browser.find(with_label 'Religion:').text.strip,
     }
 
     party_selector = with_label 'Political Party:'
     if browser.has_selector? party_selector
-      person[:party]= /.*\(([^\(]*)\)/.match(browser.find(party_selector).text.strip)[1].to_s.strip
+      person[:party_id], person[:party] = party_info(browser.find(party_selector).text.strip)
     end
 
     raw_dob = browser.find(with_label 'Date of birth:').text.strip
