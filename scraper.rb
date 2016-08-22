e621f94ce88bdf05e8a29e4342956ca62603d248
Chris Mytton <chrismytton@gmail.com>
2016-08-22 17:24:42 +0100

    Extract scrape_table_row method
    This makes the code a bit easier to follow by separating the code that
    finds the list of people from the code that processes each individual
    person.

--- scraper.rb
+++ scraper.rb
@@ -26,45 +26,45 @@ def ocd_lookup
 end
 
 def scrape_list(url,browser)
-  year = '2015'
-  people = []
   browser.visit(url)
   browser.click_button 'Show all at once'
-  browser.within( '//body/table') do
-    browser.all('./tbody/tr[position()>2 and position()<last()]').each  do |row|
-      absolute_uri = URI.join(url, row.find('./td/a')[:href]).to_s
-      person = {}
-      names = row.find('./td[position()=1]').text.strip.split(/[[:space:]]/, 2).reverse
-      person[:name] = names.join(" ")
-      person[:given_name] = names.first
-      person[:family_name] = names.last
-      person[:sort_name] = "#{names.last}, #{names.first}"
-
-      person['id'] = year + '-'+/&j=(?<id>\d*)&const/.match(absolute_uri)[:id].to_s
-      person['source'] = absolute_uri
-      person[:url] = absolute_uri
-      person[:district] = row.find('./td[position()=4]').text.strip
-      person[:constituency] = row.find('./td[position()=3]').text.strip
-
-      special = ["PWD", "YOUTH", "EX-OFFICIO", "Workers' Represantive", "Woman Representative", "UPDF", "Workers' Representative"].to_set
-      if special.include? person[:constituency]
-        spelling_fixups = {
-          "Workers' Represantive" => "Workers' Representative",
-        }
-        person[:legislative_membership_type] = spelling_fixups.fetch(person[:constituency], person[:constituency])
-        person[:constituency] = ""
-
-        person[:area_id] = ocd_lookup.find(district: person[:district].gsub(/district/i, '').strip)
-      else
-        person[:area_id] = ocd_lookup.find(district: person[:district].gsub(/district/i, '').strip, constituency: person[:constituency])
-      end
-
-      warn "Couldn't find :area_id for district=#{person[:district]} constituency=#{person[:constituency]}" if person[:area_id].nil?
-
-      people.push(person)
-    end
+  browser.find_all('//body/table/tbody/tr[position()>2 and position()<last()]').map  do |row|
+    scrape_table_row(url, row)
   end
-  return people
+end
+
+def scrape_table_row(url, row)
+  year = '2015'
+  absolute_uri = URI.join(url, row.find('./td/a')[:href]).to_s
+  person = {}
+  names = row.find('./td[position()=1]').text.strip.split(/[[:space:]]/, 2).reverse
+  person[:name] = names.join(" ")
+  person[:given_name] = names.first
+  person[:family_name] = names.last
+  person[:sort_name] = "#{names.last}, #{names.first}"
+
+  person['id'] = year + '-'+/&j=(?<id>\d*)&const/.match(absolute_uri)[:id].to_s
+  person['source'] = absolute_uri
+  person[:url] = absolute_uri
+  person[:district] = row.find('./td[position()=4]').text.strip
+  person[:constituency] = row.find('./td[position()=3]').text.strip
+
+  special = ["PWD", "YOUTH", "EX-OFFICIO", "Workers' Represantive", "Woman Representative", "UPDF", "Workers' Representative"].to_set
+  if special.include? person[:constituency]
+    spelling_fixups = {
+      "Workers' Represantive" => "Workers' Representative",
+    }
+    person[:legislative_membership_type] = spelling_fixups.fetch(person[:constituency], person[:constituency])
+    person[:constituency] = ""
+
+    person[:area_id] = ocd_lookup.find(district: person[:district].gsub(/district/i, '').strip)
+  else
+    person[:area_id] = ocd_lookup.find(district: person[:district].gsub(/district/i, '').strip, constituency: person[:constituency])
+  end
+
+  warn "Couldn't find :area_id for district=#{person[:district]} constituency=#{person[:constituency]}" if person[:area_id].nil?
+
+  person
 end
 
 def party_info(str)
